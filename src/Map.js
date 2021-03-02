c3b84d64e3e8a368a60ae76441a7fc6f8d484f94
AvidDabbler <walter.k.jenkins@gmail.com>
2021-03-01 21:19:07 -0600

    added popup location and tracking

--- src/Map.js
+++ src/Map.js
@@ -3,29 +3,29 @@ import { loadModules } from "esri-loader";
 
 export default function Map() {
     const mapEl = useRef(null)
-        // const routes = (GeoJSONLayer)=>{
-        //     return new GeoJSONLayer({
-        //         url: 'https://opendata.arcgis.com/datasets/80d7b4b8e93f43929ed345d7c72ec4c5_0.geojson',
-
-        //     })
-        // }
 
         const loadMap = () =>loadModules([
             "esri/config",
             "esri/Map",
             "esri/views/MapView",
-            "esri/layers/GeoJSONLayer"
+            "esri/layers/GeoJSONLayer",
+            "esri/widgets/Locate",
+            "esri/widgets/Track",
+            "esri/Graphic"
         ], {css: true})
-            .then(function ([esriConfig, Map, MapView, GeoJSONLayer]){
+            .then(function ([esriConfig, Map, MapView, GeoJSONLayer, Locate, Track, Graphic]){
                 esriConfig.apiKey = 'AAPKaeaf4e70e4fd4906b1257137a854cc97eVbz1ae-QH51T37UQW1XvlnO64OTwPnR9J4tnfA6uDtrLwzani_HfpOETtNobPp_'
                 
                 const routes = new GeoJSONLayer({
                         url: 'https://opendata.arcgis.com/datasets/80d7b4b8e93f43929ed345d7c72ec4c5_0.geojson',
-        
-                })
+                    popupTemplate: {
+                        title: "Routes",
+                        content: "<b>Route: </b>{RouteAbbr}"
+                        }
+                })      
 
                 const map = new Map({
-                        basemap: "arcgis-topographic", // Basemap layer service
+                        basemap: "arcgis-navigation", // Basemap layer service
                         layers: [routes]
                     });
 
@@ -35,7 +35,34 @@ export default function Map() {
                     map: map,
                     center: [-90.199402, 38.627003],
                     zoom: 13
-                    });
+                });
+                
+                const locate = new Locate({
+                    view: view,
+                    useHeadingEnabled: false,
+                    goToOverride: function(view, options) {
+                      options.target.scale = 1500;
+                      return view.goTo(options.target);
+                    }
+                });
+                const track = new Track({
+                    view: view,
+                    graphic: new Graphic({
+                      symbol: {
+                        type: "simple-marker",
+                        size: "12px",
+                        color: "green",
+                        outline: {
+                          color: "#efefef",
+                          width: "1.5px"
+                        }
+                      }
+                    }),
+                    useHeadingEnabled: false
+                  });
+
+                view.ui.add(track, "top-left");
+                view.ui.add(locate, "top-left"); 
                     
             })
     
