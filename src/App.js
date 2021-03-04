7f690fbf8266284c76ba06867fe22850327e1257
AvidDabbler <walter.k.jenkins@gmail.com>
2021-03-04 00:28:08 -0600

    google analytics and index.html updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import './tailwind.min.css'
 import Map from './Map'
-import Buttons from './Buttons'
+import Buttons from './Buttons';
+import React,{useEffect} from "react";
+import ReactGA from "react-ga";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('G-GD3RK5WKFH');
+    ReactGA.pageview(window.location.pathname);
+  })
+
   return (
     <div className="App">
       <Map />
